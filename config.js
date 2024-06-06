e841fbf64c93a3cf3363ed79ded4677fdf301758
Suranga1 <143412727+Suranga1@users.noreply.github.com>
2024-06-06 15:47:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
+global.Owner = ["+94720319480", "+94720319480"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94720319480", "+94720319480"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94720319480"];
 global.OwnerName = "KUSHAN";
 global.BotName = "DARK SHAM";
 global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
